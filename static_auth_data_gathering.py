c170f6c175a2062f2e82b63bf22aaffce150e611
Devasia Manuel <devasia1000@gmail.com>
2016-09-13 16:40:38 -0700

    initial commit

---
+++ static_auth_data_gathering.py
@@ -0,0 +1,69 @@
+import pyxhook
+import time
+import signal
+import sys
+import pickle
+
+sample_number = 0
+curr_str = ''
+
+current_milli_time = lambda: int(round(time.time() * 1000))
+
+down_events = []
+up_events = []
+
+def print_event(event, prefix):
+    if (event.Ascii >= 48 and event.Ascii <= 57) or (event.Ascii >= 65 and event.Ascii <= 90) or (event.Ascii >= 97 and event.Ascii <= 122):
+        print prefix, event.Key, current_milli_time()
+
+def record_event(event, prefix):
+    if prefix == 'KeyDown:':
+        global curr_str
+        curr_str = curr_str + event.Key
+        down_events.append([event.Key, current_milli_time()])
+    elif prefix == 'KeyUp:':
+        up_events.append([event.Key, current_milli_time()])
+
+def kb_down_event(event):
+    #print_event(event, 'KeyDown:')
+    record_event(event, 'KeyDown:')
+
+def kb_up_event(event):
+    record_event(event, 'KeyUp:')
+
+    if event.Ascii == 32:
+        global sample_number
+        global curr_str
+        global up_events
+        global down_events
+
+        if curr_str == 'passwordspace':
+            print curr_str
+            del up_events[0]
+            data = dict()
+            data['key_down_events'] = down_events
+            data['key_up_events'] = up_events
+            print data
+            pickle.dump(data, open('sample_' + str(sample_number) + '.pickle', 'wb')) 
+            #sys.exit(0)
+            print 'Saved to sample', sample_number
+            sample_number = sample_number + 1
+            print 'Incremented sample number to ', sample_number
+        else:
+            print 'Incorrect string. Skipping...'
+        
+        curr_str = ''
+        up_events = []
+        down_events = []
+
+
+hookman = pyxhook.HookManager()
+
+hookman.KeyDown = kb_down_event
+hookman.KeyUp = kb_up_event
+
+hookman.HookKeyboard()
+hookman.start()
+    
+#Close the listener when we are done
+#hookman.cancel()
